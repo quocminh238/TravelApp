2266cb295bd4936dfcffcc179b7f7c053aa74ae1
Nguyễn Quốc Minh <59008614+quocminh238@users.noreply.github.com>
2022-08-05 12:54:47 +0700

    Initial commit

---
+++ TravelApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TravelApp
+//
+//  Created by Minh, Nguyen Quoc on 29/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+//        MainScreenView()
+        WelcomeView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Minh, Nguyen Quoc on 29/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
